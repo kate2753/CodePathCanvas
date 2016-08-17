dedba9749f2dc6e2575fbbf08818499d66ef92cb
Kate Odnous <kate.odnous@airbnb.com>
2016-08-17 14:19:25 -0700

    Add tray and allow to open\close it

--- CodePathCanvas/ViewController.swift
+++ CodePathCanvas/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var trayView: UIView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    trayCenterWhenOpen = trayView.center
+    trayCenterWhenClosed = CGPoint(x: trayView.center.x, y: trayView.center.y + trayView.frame.height - 30)
     // Do any additional setup after loading the view, typically from a nib.
   }
 
@@ -21,5 +26,28 @@ class ViewController: UIViewController {
   }
 
 
+  var trayOriginalCenter: CGPoint!
+  var trayCenterWhenOpen: CGPoint!
+  var trayCenterWhenClosed: CGPoint!
+  @IBAction func onTrayPanGesture(panGestureRecognizer: UIPanGestureRecognizer) {
+    if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+      trayOriginalCenter = trayView.center
+    } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+      let translation = panGestureRecognizer.translationInView(trayView)
+      trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+    } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+      let velocity = panGestureRecognizer.velocityInView(trayView)
+      UIView.animateWithDuration(0.2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+        self.trayView.center = velocity.y > 0 ? self.trayCenterWhenClosed : self.trayCenterWhenOpen
+      }, completion: nil)
+    }
+  }
+  
+  @IBAction func onTrayTapGesture(sender: UITapGestureRecognizer) {
+    UIView.animateWithDuration(0.2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+      self.trayView.center = self.trayView.center.y == self.trayCenterWhenOpen.y ? self.trayCenterWhenClosed : self.trayCenterWhenOpen
+      }, completion: nil)
+
+  }
 }
 
