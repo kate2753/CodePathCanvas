88913f46e498706160fb85bb7b6d178144ad6da9
Kate Odnous <kate.odnous@airbnb.com>
2016-08-17 14:40:31 -0700

    Pan canvas images

--- CodePathCanvas/ViewController.swift
+++ CodePathCanvas/ViewController.swift
@@ -39,15 +39,60 @@ class ViewController: UIViewController {
       let velocity = panGestureRecognizer.velocityInView(trayView)
       UIView.animateWithDuration(0.2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
         self.trayView.center = velocity.y > 0 ? self.trayCenterWhenClosed : self.trayCenterWhenOpen
-      }, completion: nil)
+        }, completion: nil)
     }
   }
-  
+
   @IBAction func onTrayTapGesture(sender: UITapGestureRecognizer) {
     UIView.animateWithDuration(0.2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
       self.trayView.center = self.trayView.center.y == self.trayCenterWhenOpen.y ? self.trayCenterWhenClosed : self.trayCenterWhenOpen
       }, completion: nil)
 
+  }
+
+  var newlyCreatedFaceOriginalCenter: CGPoint!
+  var newlyCreatedFace: UIImageView!
+  @IBAction func onSmileyTapGesture(panGestureRecognizer: UIPanGestureRecognizer) {
+    if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+      // Gesture recognizers know the view they are attached to
+      let imageView = panGestureRecognizer.view as! UIImageView
+
+      // Create a new image view that has the same image as the one currently panning
+      newlyCreatedFace = UIImageView(image: imageView.image)
+      newlyCreatedFace.userInteractionEnabled = true
+      // Add the new face to the tray's parent view.
+      view.addSubview(newlyCreatedFace)
+
+      // Initialize the position of the new face.
+      newlyCreatedFace.center = imageView.center
+
+      // Since the original face is in the tray, but the new face is in the
+      // main view, you have to offset the coordinates
+      newlyCreatedFace.center.y += trayView.frame.origin.y
+      newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+
+      let canvasSmiliePanGesture = UIPanGestureRecognizer(target: self, action: #selector(ViewController.onCanvasSmileyPanGesture))
+      newlyCreatedFace.addGestureRecognizer(canvasSmiliePanGesture)
+
+    } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+      let translation = panGestureRecognizer.translationInView(panGestureRecognizer.view)
+      newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+    }
+  }
+
+  var imageViewOriginalCenter: CGPoint!
+  func onCanvasSmileyPanGesture(panGestureRecognizer: UIPanGestureRecognizer) {
+    let imageView = panGestureRecognizer.view as! UIImageView
+
+    if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+      imageView.frame = CGRect(origin: imageView.frame.origin, size: CGSize(width: imageView.frame.width + 5, height: imageView.frame.height + 5))
+      imageViewOriginalCenter = imageView.center
+    } else if panGestureRecognizer.state == .Changed {
+      let translation = panGestureRecognizer.translationInView(panGestureRecognizer.view)
+      imageView.center = CGPoint(x: imageViewOriginalCenter.x + translation.x, y: imageViewOriginalCenter.y + translation.y)
+    } else if panGestureRecognizer.state == .Ended {
+      imageView.frame = CGRect(origin: imageView.frame.origin, size: CGSize(width: imageView.frame.width - 5, height: imageView.frame.height - 5))
+    }
   }
 }
 
